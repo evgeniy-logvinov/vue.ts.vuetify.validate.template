ac88076cdbc15de8f5a97b9c56246dda34507e0c
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2020-10-07 14:06:04 +0300

    Styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    plugins: {
-        'postcss-nested': {},
-        'postcss-responsive-type': {},
-        'postcss-hexrgba': {},
-        'postcss-preset-env': {},
-    },
-    preset: {
-        stage: 0,
-        autoprefixer: {},
-        importFrom: './static/styles/variables.css',
-        preserve: false,
-    },
+  plugins: {
+    // 'postcss-nested': {},
+    // 'postcss-responsive-type': {},
+    // 'postcss-hexrgba': {},
+    // 'postcss-preset-env': {
+    //   stage: 2,
+    // },
+  },
+  preset: {
+    stage: 0,
+    autoprefixer: {},
+    importFrom: './static/styles/variables.css',
+    preserve: false,
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import './vee-validate';
+import '@/static/styles/variables.css';
+import '@/static/styles/fonts.css';
+import '@/static/styles/global.css';
+import '@/static/styles/reset.css';
 
 // Register it globally
 Vue.component('ValidationProvider', ValidationProvider);
